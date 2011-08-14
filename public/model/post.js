11fe32b53021b2f9c1d6c8723abe3639bd4889e6
Raynos <raynos2@gmail.com>
2011-08-14 23:53:17 +0100

    Cleaned up CSS, refactored database code into model

--- public/model/post.js
+++ public/model/post.js
@@ -1,12 +1,133 @@
-var Model = require("LazyBoy");
+var request = require("request").defaults({
+		"headers": {
+			"Authorization": "Basic " + 
+				new Buffer(process.env.COUCH_USER + ":" + 
+				process.env.COUCH_PWD).toString("base64")
+		}
+	}),
+	url = require("url"),
+	uuid = require("node-uuid");
 
-var Post = Model.define("blog_post", {
-	"content": String,
-	"title": String,
-	"datetime": Number,
-	"url": String
-});
+var Post = {
+	"_cache": {},
+	"_base_url": "http://raynos.iriscouch.com/raynos",
+	"_error": function _error (f) {
+		return function _errorProxy(err, res, body) {
+			if (err) {
+				console.log("error in blog");
+				console.log(err);
+			} else {
+				f(err, res, body);
+			}
+		};	
+	},
+	"_cloneRows": function _cloneRows(rows) {
+		return rows.map(function _cloneRows(row) { 
+			var o = { 
+				value: {}
+			};
+			Object.keys(row.value).forEach(function _cloneValues(key) {
+				o.value[key] = row.value[key];
+			});
+			return o;
+		});
+	},
+	"get": function _get(id, cb) {
+		if (typeof id === "function") {
+			cb = id; 
+			id = null;
+		}
 
-Post.beforeCreate(function(p) {
-	p.url = p.title.split(" ").join("-");
-});
+		if (id !== null && this._cache[id]) {
+			cb(null, this._cloneRows(this._cache[id]));
+			return;
+		};
+
+		var uri = url.parse(this._base_url)
+		uri.pathname += "/_design/posts/_view/all";
+		uri.query = {
+			"descending": JSON.stringify(true)
+		};
+		if (id) {
+			uri.query.startkey = uri.query.endkey = JSON.stringify(id);
+		}
+
+		request({
+			"uri": url.format(uri)
+		}, this._error((function _callback(err, resp, body) {
+			var rows = JSON.parse(body).rows;
+			if (!rows || rows.length === 0) {
+				cb(new Error("no results"), []);
+			} else {
+				if (id) {
+					this._cache[id] = this._cloneRows(rows);
+				}
+				cb(null, rows);
+			}
+		}).bind(this)));
+	},
+	"save": function _save(data, cb) {
+		this.get(data.id, (function _saveData(err, rows) {
+			delete this._cache[data.id];
+
+			Object.keys(data).forEach(function _cloneOverValues(key) {
+				rows[0].value[key] = data[key];
+			});
+
+			request({
+				"uri": this._base_url + "/" + rows[0].value._id,
+				"json": rows[0].value,
+				"method": "PUT"
+			}, this._error(cb));
+		}).bind(this));
+	},
+	"create": function _create(data, cb) {
+		data._id = uuid();
+
+		request({
+			"uri": this._base_url + "/" + data._id,
+			"json": data,
+			"method": "PUT"
+		}, this._error(cb));
+	},
+	"destroy": function _destroy(id, cb) {
+		this.get(id, (function _destroyDoc(err, rows) {
+			delete this._cache[id];
+			var uri = url.parse(this._base_url);
+			uri.pathname += "/" + rows[0].value._id;
+			uri.query = {
+				"rev": rows[0].value._rev
+			};
+
+			request({
+				"url": url.format(uri),
+				"method": "DELETE"
+			}, this._error(cb));	
+		}).bind(this));
+	}
+};
+
+// Create all view
+request({
+	"url": Post._base_url + "/_design/posts"
+}, Post._error(function _callback(err, resp, body) {
+	// If it does not exist create the view
+	if (JSON.parse(body).error === "not_found") {
+		var view = {
+			"all": {
+				"map": "function(d) { if (d.type === 'post') emit(d.id, d); }"
+			}
+		};
+
+		request({
+			"url": Post._base_url + "/_design/posts",
+			"method": "PUT",
+			"json": {
+				"_id": "_design/posts",
+				"views": view
+			}
+		}, Post._error(function _noop() {}));
+	}
+}));
+
+module.exports = Post;
--- routes/blog.js
+++ routes/blog.js
@@ -1,113 +1,30 @@
 //var posts = require("../public/model/posts.js"),
 var markdown = require("markdown").markdown,
-	r = require("request").defaults({
-		"headers": {
-			"Authorization": "Basic " + 
-				new Buffer(process.env.COUCH_USER + ":" + 
-				process.env.COUCH_PWD).toString("base64")
-		}
-	}),
-	uuid = require("node-uuid");
-
-
-var base_url = "http://raynos.iriscouch.com/raynos";
+	Posts = require("../public/model/post.js");
 
 var createURL = function _createURL(p) {
 	return p.id + "/" + encodeURIComponent(p.title.replace(/\s/g, "-"));
 }
 
 var fixURL = function _fixURL(p) {
+	p = (p[0] || p).value;
 	p.url = createURL(p);
 	return p;
 }
 
 var view = function _view(p) {
+	p = fixURL(p);
 	p.readable_time = new Date(p.datetime).toDateString()
 	p.content = markdown.toHTML(p.content);
-	fixURL(p);
 	return p;
 };
-
-var error = function _error(f) {
-	return function _errorProxy(err, res, body) {
-		if (err) {
-			console.log("error in blog");
-			console.log(err);
-		} else {
-			f(err, res, body);
-		}
-	};
-};
-
-var noop = error(function _noop() {});
-
-r({
-	"url": base_url + "/_design/posts"
-}, error(function _callback(err, resp, body) {
-	if (JSON.parse(body).error === "not_found") {
-		var view = {
-			"all": {
-				"map": "function(d) { if (d.type === 'post') emit(d.id, d); }"
-			}
-		};
-
-		r({
-			"url": base_url + "/_design/posts",
-			"method": "PUT",
-			"json": {
-				"_id": "_design/posts",
-				"views": view
-			}
-		}, noop);
-	}
-}));
-
-var get = function(id, cb) {
-	if (typeof id === "function") {
-		cb = id; 
-		id = null;
-	}
-
-	var uri = base_url + "/_design/posts/_view/all";
-	if (id) {
-		uri += "?startkey=" + 
-			encodeURIComponent(JSON.stringify(id)) + 
-			"&endkey=" + 
-			encodeURIComponent(JSON.stringify(id));
-	}
-
-	r({
-		"uri": uri
-	}, error(function _callback(err, resp, body) {
-		var rows = JSON.parse(body).rows;
-		if (!rows || rows.length === 0) {
-			cb(new Error("no results"), []);
-		} else if (id !== null) {
-			cb(null, rows[0].value);	
-		} else {
-			cb(null, rows);
-		}
-	}));
-}
-
-var save = function(data, id, cb) {
-	r({
-		"uri": base_url + "/" + id,
-		"json": data,
-		"method": "PUT"
-	}, error(function _callback(err, resp, body) {
-		cb();
-	}));
-}
 
 module.exports = function _route(app) {
 
 	app.get("/blog", function _index(req, res) {
-		get(function _get(err, data) {
+		Posts.get(function _get(err, data) {
 			res.render("blog/index", {
-				"posts": data.map(function(p) {
-					return view(p.value);
-				})
+				"posts": data.map(view)
 			});
 		});
 	});
@@ -124,16 +41,16 @@ module.exports = function _route(app) {
 			"type": "post"
 		}
 
-		get(function _get(err, rows) {
-			var id = rows.map(function(v) {
+		Posts.get(function _get(err, rows) {
+			var id = rows.map(function _pluckId(v) {
 				return v.value.id;
-			}).reduce(function(prev, curr) {
+			}).reduce(function _findMaxId(prev, curr) {
 				return prev < curr ? curr : prev;
 			}, 0);
 
 			data.id = ++id;
 
-			save(data, uuid(), function _save() {
+			Posts.create(data, function _save() {
 				res.redirect("blog/" + createURL(data));
 			});		
 		});
@@ -142,19 +59,19 @@ module.exports = function _route(app) {
 	app.get("/blog/:id/edit", function _edit(req, res) {
 		var id = +req.params.id
 		
-		get(id, function _get(err, val) {
-			res.render("blog/edit", fixURL(val));
+		Posts.get(id, function _get(err, rows) {
+			res.render("blog/edit", fixURL(rows));
 		});
 	});
 
 	app.get("/blog/:id/:title", function _show(req, res) {
 		var id = +req.params.id;
 
-		get(id, function _get(err, val) {
+		Posts.get(id, function _get(err, rows) {
 			if (err && err.message === "no results") {
 				res.redirect("404");
 			} else {
-				res.render("blog/show", view(val));	
+				res.render("blog/show", view(rows));	
 			}
 		});
 	});
@@ -162,28 +79,22 @@ module.exports = function _route(app) {
 	app.put("/blog/:id", function _update(req, res) {
 		var id = +req.params.id;
 		
-		get(id, function _get(err, val) {
-			val.title = req.body.title;
-			val.content = req.body.content;
+		var val = {
+			"title": req.body.title,
+			"content": req.body.content,
+			"id": id
+		};
 
-			save(val, val._id, function _save() {
-				res.redirect("blog/" + createURL(val));
-			});
+		Posts.save(val, function _save() {
+			res.redirect("blog/" + createURL(val));
 		});
 	});
 
 	app.delete("/blog/:id", function _destroy(req, res) {
 		var id = +req.params.id;
 
-		get(id, function _get(err, val) {
-			var uri = base_url + "/" + val._id + "?rev=" + val._rev;
-			
-			r({
-				"url": uri,
-				"method": "DELETE"
-			}, function _callback() {
-				res.redirect("/blog/")
-			});
+		Posts.destroy(id, function _delete() {
+			res.redirect("/blog/")
 		});
 	});
 
