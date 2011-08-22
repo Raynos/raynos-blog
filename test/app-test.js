d360301a45778297bfa31890a27a1b4f60fe0548
Raynos <raynos2@gmail.com>
2011-08-22 21:47:33 +0100

    added some unit tests

---
+++ test/app-test.js
@@ -0,0 +1,91 @@
+var is = require("vows-is");
+
+is.config({
+	"server": {
+		"factory": require("../server.js"),
+		"uri": "http://localhost:" + (process.env.PORT || 8080),
+		"defaults": {
+			"followRedirect": false
+		}
+	}
+});
+
+is.partial("redirect", function _partial(context, options) {
+	return context
+		.vow.it.should.not.error
+		.vow.it.should.have.status(302)
+
+		.partial("body")
+			.vow.it.should.include.string(options.text)
+			.vow.it.should.include.string("Moved Temporarily")
+			.suite();
+});
+
+is.partial("200", function _partial(context) {
+	return context
+		.vow.it.should.not.error
+		.vow.it.should.have.status(200)
+		.vow.it.should.have.header("content-type", "text/html; charset=utf-8");
+});
+
+is.partial("body", function _partial(context) {
+	return context.context("contains a body that")
+		.topic.is.property("body")
+		.vow.it.should.be.ok;
+})
+
+is.suite("app").batch()
+
+	.context("a request to GET /")
+		.topic.is.a.request("GET /")
+		.partial("redirect", {
+			"text": "blog"
+		})
+
+.batch()
+
+	.context("a request to GET /blog")
+		.topic.is.a.request("GET /blog")
+		.partial("200")
+
+		.partial("body")
+			.vow.it.should.include.string("blog")
+			.vow.it.should.include.string("Raynos")
+			.suite()
+
+.batch()
+
+	.context("a request to POST /blog")
+		.topic.is.a.request("POST /blog")
+		.partial("redirect", {
+			"text": "login"
+		})
+
+.batch()
+
+	.context("a request to GET /blog/1/edit")
+		.topic.is.a.request("GET /blog/0/edit")
+		.partial("redirect", {
+			"text": "login"
+		})
+
+.batch()
+
+	.context("a request to GET /blog/new")
+		.topic.is.a.request("GET /blog/new")
+		.partial("redirect", {
+			"text": "login"
+		})
+
+.batch()
+
+	.context("a request to GET /blog/1")
+		.topic.is.a.request("GET /blog/1")
+		.partial("200")
+
+		.partial("body")
+			.vow.it.should.include.string("blog")
+			.vow.it.should.include.string("Building a blog with node.js")
+			.suite()
+
+.export(module);
