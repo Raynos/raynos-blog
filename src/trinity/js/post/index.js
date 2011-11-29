86446556acc1d8c8e78cf833458f2e52dd685dc9
Raynos <raynos2@gmail.com>
2011-11-29 22:18:51 +0000

    updated html

--- src/trinity/js/post/index.js
+++ src/trinity/js/post/index.js
@@ -8,13 +8,15 @@ if (!data.user) {
 	frag.removeChild(form);
 }
 
+var link = frag.childNodes[2];
+
 data.forEach(function (post, index) {
 	var itemFrag = load("post/item", post);
 	var article = itemFrag.firstChild;
 	var footer = load("post/item-footer", post);
 	article.appendChild(footer);
-	frag.appendChild(article);
+	frag.insertBefore(article, link);
 	if (index !== data.length - 1) {
-		frag.appendChild(document.createElement("hr"));	
+		frag.insertBefore(document.createElement("hr"), link);	
 	}
 });
